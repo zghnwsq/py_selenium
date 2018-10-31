463713fda80935703acb2a9737043aa6909efd36
zghnwsq <tedwang@sina.cn>
2018-10-31 20:55:34 +0800

    push test

--- test1.py
+++ test1.py
@@ -95,7 +95,7 @@ class MyTestCase(unittest.TestCase):
         #     print params[5].encode('utf-8')
 
         result = executetest.ExecuteTest().execute2('/Users/ted/Documents/testlog/testCase1.xlsx', 'sheet1')
-        self.assertEqual(True, result, u'用例执行失败，请查看日志')
+        self.assertEqual(True, result, u'用例执行失败，请查看日志')  #111
 
 
 if __name__ == '__main__':
