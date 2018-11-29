083b1ed7d3586c2b730d6a4d988a95c986d9d22d
ted <tedwang@sina.cn>
2018-11-29 11:04:13 +0800

    增加用例集组织示例

---
+++ paramTest/run_all.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+
+import unittest
+
+# import TestLogin
+
+# 跨.py组织用例集
+
+if __name__ == '__main__':
+    # suite = unittest.TestSuite()
+    # suite.addTest(TestLogin.TestLogin('test_a'))
+    # runner = unittest.TextTestRunner(verbosity=2)
+    # runner.run(suite)
