563f56cbfc6ce54f141649cb8f2c18144b19973c
ted <tedwang@sina.cn>
2018-11-29 13:54:51 +0800

    修改后的HTMLTestRunnerCN

--- paramTest/run_all.py
+++ paramTest/run_all.py
@@ -7,7 +7,7 @@ import unittest
 # 跨.py组织用例集
 
 if __name__ == '__main__':
-    # suite = unittest.TestSuite()
+    suite = unittest.TestSuite()
     # suite.addTest(TestLogin.TestLogin('test_a'))
     # runner = unittest.TextTestRunner(verbosity=2)
     # runner.run(suite)
