320256c86df3ad5ea798028734ff70d0230c7c0f
ted <tedwang@sina.cn>
2018-12-25 11:12:11 +0800

    修复下路径问题3

--- paramTest/testPhpwind.py
+++ paramTest/testPhpwind.py
@@ -194,7 +194,7 @@ if __name__ == '__main__':
     # fileBase = 'C:/Users/Administrator/PycharmProjects/selenium'
     # 利用web服务,提供报告的网页浏览
     # fileBase = 'C:/wamp/www'
-    fileBase = './'  # 分布式执行报告目录
+    fileBase = '.'  # 分布式执行报告目录
     runner = HTMLTestRunnerCN.HTMLTestRunner(
         stream=fileBase,
         title='{ Test Report }',
